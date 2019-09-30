08457ea561a0b3d3e1a62d9e44333303f8d7b4f9
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-09-30 04:26:13 +0300

    Add a user array to the user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,4 @@
 export class User {
-}
+    constructor(public id:number, public name:string, public picture:string, public followers:number, public following:number, public repositories:number, public creationDate:string){}
+  }
+  
