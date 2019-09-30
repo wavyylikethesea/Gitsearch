d92f2898529870046b3f5087f44f13e06271e66c
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-09-30 04:29:51 +0300

    Add the repository array to the class

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,4 @@
 export class Repository {
-}
+    constructor(public repoId:number,public repoName:string,public description:string, public language:string, public forks:number, public url:string){}
+  }
+  
