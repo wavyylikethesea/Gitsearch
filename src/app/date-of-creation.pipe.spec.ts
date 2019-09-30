fecc710912ff3c596862d05ebae231cf2591b7d5
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-09-30 04:45:22 +0300

    Create the date fo

---
+++ src/app/date-of-creation.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DateOfCreationPipe } from './date-of-creation.pipe';
+
+describe('DateOfCreationPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateOfCreationPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
