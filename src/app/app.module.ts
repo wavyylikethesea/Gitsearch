6317c431ecd864fe80b694806dc1a8c5d1962afa
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-09-30 04:35:04 +0300

    Create my profile's component which will contrain my repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    MyProfileComponent
   ],
   imports: [
     BrowserModule,
