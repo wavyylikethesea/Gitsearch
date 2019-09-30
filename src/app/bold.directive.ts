283cb56b955f2682baeb6e143378b2b311e21dab
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-09-30 04:52:49 +0300

    Add images to the assets folder

--- src/app/bold.directive.ts
+++ src/app/bold.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appBold]'
 })
 export class BoldDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) {
+   elem.nativeElement.style.fontWeight = "bold";
+ }
 
 }
--- src/app/date-of-creation.pipe.ts
+++ src/app/date-of-creation.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateOfCreationPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any): any {
+    let valueArr = value.replace(/(\d{4})-(\d{2})-(\d{2}).*/ig,"$1 $2 $3").split(" ");
+    let dateCreated = new Date(valueArr[0],valueArr[1]-1, valueArr[2]);
+    let now = new Date() ;
+    let diff = now.getTime() - dateCreated.getTime();
+    diff/=(1000 * 60 * 60 * 24);
+    return Math.floor(diff);
   }
 
 }
